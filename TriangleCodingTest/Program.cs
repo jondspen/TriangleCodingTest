2639457c3ff8a2c429334a0c158003b31bb69805
Jon Spencer <jondspen@gmail.com>
2017-09-18 20:27:04 -0500

    Add project files.

---
+++ TriangleCodingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriangleCodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
